53c6722d9df696606efdde2786294f4bccf7ef1a
yuta-otsubo <yuta.otsubo@sencorp.co.jp>
2025-05-29 08:22:19 +0900

    文字化けするので英語にする

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import random
 from pygame.locals import *
 import amazon_style
 
-# 定数
+# Constants
 BOARD_SIZE = 4
 TILE_SIZE = 100
 WINDOW_WIDTH = BOARD_SIZE * TILE_SIZE
-WINDOW_HEIGHT = BOARD_SIZE * TILE_SIZE + 50  # ボタン用の余白
+WINDOW_HEIGHT = BOARD_SIZE * TILE_SIZE + 50  # Extra space for buttons
 FPS = 30
 
-# 方向定数
+# Direction constants
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
@@ -22,40 +22,40 @@ class SlidingPuzzle:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Amazon スライドパズル')
+        pygame.display.set_caption('Amazon Sliding Puzzle')
         self.basic_font = pygame.font.SysFont('Arial', 20)
         self.title_font = pygame.font.SysFont('Arial', 30, bold=True)
         
-        # Amazonスタイルの背景
+        # Amazon style background
         self.background = amazon_style.create_amazon_background(WINDOW_WIDTH, WINDOW_HEIGHT)
         
-        # ボタンの設定
-        self.reset_button = amazon_style.create_amazon_button(100, 30, 'リセット', self.basic_font)
+        # Button setup
+        self.reset_button = amazon_style.create_amazon_button(100, 30, 'Reset', self.basic_font)
         self.reset_rect = self.reset_button.get_rect(topleft=(10, WINDOW_HEIGHT - 40))
         
-        self.new_button = amazon_style.create_amazon_button(100, 30, '新規', self.basic_font)
+        self.new_button = amazon_style.create_amazon_button(100, 30, 'New', self.basic_font)
         self.new_rect = self.new_button.get_rect(topleft=(120, WINDOW_HEIGHT - 40))
         
-        self.solve_button = amazon_style.create_amazon_button(100, 30, '解く', self.basic_font)
+        self.solve_button = amazon_style.create_amazon_button(100, 30, 'Solve', self.basic_font)
         self.solve_rect = self.solve_button.get_rect(topleft=(230, WINDOW_HEIGHT - 40))
         
-        # Amazonロゴ
+        # Amazon logo
         self.logo = amazon_style.create_amazon_logo(100, 30)
         self.logo_rect = self.logo.get_rect(topleft=(WINDOW_WIDTH - 110, WINDOW_HEIGHT - 40))
         
-        # タイルの画像を事前に生成
+        # Pre-generate tile images
         self.tile_images = {}
         for i in range(1, BOARD_SIZE * BOARD_SIZE):
             self.tile_images[i] = amazon_style.create_amazon_tile(TILE_SIZE, i, self.basic_font)
         
-        # ボードの初期化
+        # Initialize board
         self.solved_board = self.get_starting_board()
-        self.main_board = self.get_starting_board()  # 先に初期化
+        self.main_board = self.get_starting_board()  # Initialize first
         self.all_moves = []
         self.main_board, self.solution_seq = self.generate_new_puzzle(80)
     
     def get_starting_board(self):
-        # 解決された状態のボードを返す
+        # Return a solved board
         counter = 1
         board = []
         for x in range(BOARD_SIZE):
@@ -70,7 +70,7 @@ class SlidingPuzzle:
         return board
     
     def get_blank_position(self, board):
-        # 空白の位置を返す
+        # Return the position of the blank space
         for x in range(BOARD_SIZE):
             for y in range(BOARD_SIZE):
                 if board[x][y] is None:
@@ -78,7 +78,7 @@ class SlidingPuzzle:
         return None
     
     def make_move(self, board, move):
-        # 指定された方向に移動
+        # Make the specified move
         blankx, blanky = self.get_blank_position(board)
         
         if move == UP:
@@ -91,7 +91,7 @@ class SlidingPuzzle:
             board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
     
     def is_valid_move(self, board, move):
-        # 指定された方向への移動が有効かどうかをチェック
+        # Check if the specified move is valid
         blankx, blanky = self.get_blank_position(board)
         return (move == UP and blanky != BOARD_SIZE - 1) or \
                (move == DOWN and blanky != 0) or \
@@ -99,10 +99,10 @@ class SlidingPuzzle:
                (move == RIGHT and blankx != 0)
     
     def get_random_move(self, board, last_move=None):
-        # ランダムな移動を取得（前回の移動と逆の移動は避ける）
+        # Get a random move (avoiding the opposite of the last move)
         valid_moves = [UP, DOWN, LEFT, RIGHT]
         
-        # 前回の移動と逆の移動を削除
+        # Remove the opposite of the last move
         if last_move == UP or not self.is_valid_move(board, DOWN):
             if DOWN in valid_moves:
                 valid_moves.remove(DOWN)
@@ -122,7 +122,7 @@ class SlidingPuzzle:
         return random.choice(valid_moves)
     
     def get_spot_clicked(self, x, y):
-        # クリックされた位置のタイルを取得
+        # Get the tile clicked
         for tilex in range(BOARD_SIZE):
             for tiley in range(BOARD_SIZE):
                 left, top = self.get_left_top_of_tile(tilex, tiley)
@@ -132,42 +132,42 @@ class SlidingPuzzle:
         return (None, None)
     
     def get_left_top_of_tile(self, tilex, tiley):
-        # タイルの左上の座標を取得
+        # Get the top left coordinates of a tile
         left = tilex * TILE_SIZE
         top = tiley * TILE_SIZE
         return (left, top)
     
     def draw_board(self, message):
-        # 背景を描画
+        # Draw the background
         self.display_surf.blit(self.background, (0, 0))
         
-        # ボードを描画
+        # Draw the board
         for tilex in range(BOARD_SIZE):
             for tiley in range(BOARD_SIZE):
                 if self.main_board and self.main_board[tilex][tiley]:
                     left, top = self.get_left_top_of_tile(tilex, tiley)
                     self.display_surf.blit(self.tile_images[self.main_board[tilex][tiley]], (left, top))
         
-        # ボードの枠線
+        # Draw the board border
         left, top = self.get_left_top_of_tile(0, 0)
         width = BOARD_SIZE * TILE_SIZE
         height = BOARD_SIZE * TILE_SIZE
         pygame.draw.rect(self.display_surf, amazon_style.AMAZON_ORANGE, (left - 2, top - 2, width + 4, height + 4), 2)
         
-        # メッセージを描画
+        # Draw the message
         if message:
             text_surf = self.title_font.render(message, True, amazon_style.AMAZON_ORANGE)
             text_rect = text_surf.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT - 25))
             self.display_surf.blit(text_surf, text_rect)
         
-        # ボタンを描画
+        # Draw the buttons
         self.display_surf.blit(self.reset_button, self.reset_rect)
         self.display_surf.blit(self.new_button, self.new_rect)
         self.display_surf.blit(self.solve_button, self.solve_rect)
         self.display_surf.blit(self.logo, self.logo_rect)
     
     def slide_animation(self, direction, message, animation_speed):
-        # タイルのスライドアニメーション
+        # Slide animation for tiles
         blankx, blanky = self.get_blank_position(self.main_board)
         if direction == UP:
             movex = blankx
@@ -182,16 +182,16 @@ class SlidingPuzzle:
             movex = blankx - 1
             movey = blanky
         
-        # アニメーションの準備
+        # Prepare for animation
         self.draw_board(message)
         base_surf = self.display_surf.copy()
         
-        # 移動するタイル
+        # Moving tile
         move_tile = self.main_board[movex][movey]
         move_left, move_top = self.get_left_top_of_tile(movex, movey)
         
         for i in range(0, TILE_SIZE, animation_speed):
-            # アニメーションフレームを描画
+            # Draw animation frame
             self.display_surf.blit(base_surf, (0, 0))
             
             if direction == UP:
@@ -207,11 +207,11 @@ class SlidingPuzzle:
             self.clock.tick(FPS)
     
     def generate_new_puzzle(self, num_slides):
-        # 新しいパズルを生成
+        # Generate a new puzzle
         sequence = []
         board = self.get_starting_board()
         
-        # 初期状態のボードを設定
+        # Set initial board state
         self.main_board = board
         
         self.draw_board('')
@@ -222,9 +222,9 @@ class SlidingPuzzle:
         for i in range(num_slides):
             move = self.get_random_move(board, last_move)
             if move is None:
-                # 有効な移動がない場合はスキップ
+                # Skip if no valid moves
                 continue
-            self.slide_animation(move, 'パズル生成中...', int(TILE_SIZE / 3))
+            self.slide_animation(move, 'Generating puzzle...', int(TILE_SIZE / 3))
             self.make_move(board, move)
             sequence.append(move)
             last_move = move
@@ -232,7 +232,7 @@ class SlidingPuzzle:
         return (board, sequence)
     
     def reset_animation(self, all_moves):
-        # リセットアニメーション
+        # Reset animation
         rev_all_moves = all_moves[:]
         rev_all_moves.reverse()
         
@@ -249,16 +249,16 @@ class SlidingPuzzle:
             self.make_move(self.main_board, opposite_move)
     
     def run(self):
-        # メインゲームループ
+        # Main game loop
         running = True
         msg = ''
         
         while running:
             slide_to = None
             
-            # ボードが解かれたかチェック
+            # Check if the board is solved
             if self.main_board == self.solved_board:
-                msg = 'クリア!'
+                msg = 'Solved!'
             
             self.draw_board(msg)
             
@@ -269,7 +269,7 @@ class SlidingPuzzle:
                     spotx, spoty = self.get_spot_clicked(event.pos[0], event.pos[1])
                     
                     if (spotx, spoty) == (None, None):
-                        # ボタンがクリックされたかチェック
+                        # Check if buttons were clicked
                         if self.reset_rect.collidepoint(event.pos):
                             self.reset_animation(self.all_moves)
                             self.all_moves = []
@@ -283,7 +283,7 @@ class SlidingPuzzle:
                             self.all_moves = []
                             msg = ''
                     else:
-                        # タイルがクリックされた
+                        # Tile was clicked
                         blankx, blanky = self.get_blank_position(self.main_board)
                         if spotx == blankx + 1 and spoty == blanky:
                             slide_to = LEFT
@@ -295,7 +295,7 @@ class SlidingPuzzle:
                             slide_to = DOWN
                 
                 elif event.type == KEYUP:
-                    # キーボード入力
+                    # Keyboard input
                     if event.key in (K_LEFT, K_a) and self.is_valid_move(self.main_board, LEFT):
                         slide_to = LEFT
                     elif event.key in (K_RIGHT, K_d) and self.is_valid_move(self.main_board, RIGHT):
@@ -305,7 +305,7 @@ class SlidingPuzzle:
                     elif event.key in (K_DOWN, K_s) and self.is_valid_move(self.main_board, DOWN):
                         slide_to = DOWN
                     elif event.key == K_r:
-                        # リセット
+                        # Reset
                         self.main_board, self.solution_seq = self.generate_new_puzzle(80)
                         self.all_moves = []
                         msg = ''
@@ -316,7 +316,7 @@ class SlidingPuzzle:
                 self.slide_animation(slide_to, '', 8)
                 self.make_move(self.main_board, slide_to)
                 self.all_moves.append(slide_to)
-                msg = ''  # 移動したらメッセージをクリア
+                msg = ''  # Clear message after move
             
             pygame.display.update()
             self.clock.tick(FPS)
